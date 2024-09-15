8e2c5d7546afd3ab09780cfa1af60aee015378df
Félix Mainville <felix@mainville.ca>
2024-09-15 00:24:50 -0400

    Added function to calculate shortest distance from shop

--- src/distance_calculator.py
+++ src/distance_calculator.py
@@ -16,6 +16,24 @@ else:
     print("API key not found")
     exit()
 
+def shortest_distance_from_shop(geocoded_array):
+
+    shop_geocode = [42.341764, -82.002213] # Hardcode the shop geo location
+    distances_from_shop =[] # array that will store distances from each property 
+
+    for i in range(0, len(geocoded_array)):
+        distance = gmaps.distance_matrix(tuple(shop_geocode), tuple(geocoded_array[i]), mode="driving") # calculate distance 
+
+        if distance['rows'][0]['elements'][0]['status'] == 'OK':
+            distances_from_shop.append(distance['rows'][0]['elements'][0]['distance']['value'])
+        else:
+            print("Distance from shop could not be found")
+            distances_from_shop.append(-1)
+        
+    smallest_distance = distances_from_shop.index(min(distances_from_shop)) # Store the index of the property with smallest distance from shop
+
+    return smallest_distance # Return the index of property with shortest distance
+
 def distance_calculator(geocoded_array):
     distance_matrix = [] # Matrix that will contain distances between all properties -  This is a hollow matrix (Diagonal is all zeroes)
 
@@ -29,6 +47,7 @@ def distance_calculator(geocoded_array):
                 meters_distance_at_i.append(time_distance_at_i['rows'][0]['elements'][0]['distance']['value']) # Extract the distance in meters 
             else:
                 print('Distance could not be calculated')
+                meters_distance_at_i.append(-1) # Append negative distance to show faulty calculation
 
         distance_matrix.append(meters_distance_at_i) # Load the distance/times comparisons from location a to all other locations into distance matrix
 
@@ -37,4 +56,6 @@ def distance_calculator(geocoded_array):
 array = [[34.0770948, -118.39719],[42.4264444, -82.1957029],[42.4042706, -82.1766639],[42.411223, -73.525481],[37.595558, -77.583413]]
 
 matrix = distance_calculator(array)
+shortest = shortest_distance_from_shop(array)
 print(matrix)
+print(shortest)
